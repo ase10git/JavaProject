0811560dc5815eca172c6656a02bf9081706b896
Jinseong0616 <81826156+Jinseong0616@users.noreply.github.com>
2023-11-14 21:26:02 +0900

    11/14

---
+++ Kiosk/src/Kisok/Menu.java
@@ -0,0 +1,23 @@
+package Kisok;
+
+public class Menu {
+	private String num;
+	private String name;
+	private String detail;
+	private int price;
+	
+	public String getNum() {
+		return this.num;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	public String getDetail() {
+		return this.detail;
+	}
+	private int getPrice() {
+		return this.price;
+	}
+	
+}
