72c385a7e14309a0a01fd06c95325ddbcaa9bf73
ase10git <148038808+ase10git@users.noreply.github.com>
2023-11-14 23:25:36 +0900

    Add files via upload

---
+++ Kiosk/src/Menu4GUI.java
@@ -0,0 +1,19 @@
+package Kisok;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Menu4GUI extends JPanel {
+	public Menu4GUI() {
+		setLayout(null);
+		
+		JLabel test4 = new JLabel();
+		add(test4);
+		
+		setBounds(100, 100, 500, 700);
+		setVisible(true);
+		setBackground(Color.WHITE);	
+	}
+}
