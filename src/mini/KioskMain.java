35a93581ca58a270fefbe3b7c64bd2fe228cbf52
Jinseong0616 <81826156+Jinseong0616@users.noreply.github.com>
2023-11-14 14:21:59 +0900

    11.14test

---
+++ src/mini/KioskMain.java
@@ -0,0 +1,31 @@
+package mini;
+
+import javax.swing.JFrame;
+
+public class KioskMain {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		f.setBounds(100,100,600,600);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
