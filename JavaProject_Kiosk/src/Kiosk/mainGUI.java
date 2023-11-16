8b6711601a7d13384c90842b05e2a82647806ce3
ase10git <148038808+ase10git@users.noreply.github.com>
2023-11-17 00:53:16 +0900

    20231117 0시 52분 소스코드 합침
    메뉴 ui 통합

--- JavaProject_Kiosk/src/Kiosk/mainGUI.java
+++ JavaProject_Kiosk/src/Kiosk/mainGUI.java
@@ -1,10 +1,10 @@
 package Kiosk;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -25,8 +25,11 @@ public class MainGUI extends JFrame {
 	static final int frameWidth = 800;
 	static final int frameHeight = 900;
 	static Font kfont = new Font("", Font.PLAIN, 18);
+	static Font mfont = new Font("", Font.PLAIN, 16);
+	
 	int numOfMenu = 4;
 	int numOfPayment = 4;
+	String imageAddr = "src/Kiosk/Images/";
 	
 	JPanel takeOutOrEat;
 	JPanel menuGUI;
@@ -200,96 +203,277 @@ public class MainGUI extends JFrame {
 	// 메뉴 종류 화면 추가 ---------------------------------------------------------
 	public JPanel addMenu1() { // 분식
 		
-		JPanel menu1 = new JPanel(new GridLayout(3, 2));
-		menu1.setBounds(0, 100, 800, 700);
-		menu1.setBackground(Color.BLUE);
-		menu1.setVisible(true);
+		JPanel menu1 = new JPanel(null);
+		String menu_snack[] = {"김밥","참치김밥","치즈김밥","쫄면","떡볶이","떡볶이","떡볶이"};		
 		
-		// 분식 메뉴 버튼 생성
-		JButton[] menuSnack = new JButton[4];
-		menuSnack[0] = new JButton("김밥");
-		menuSnack[1] = new JButton("라면");
-		menuSnack[2] = new JButton("쫄면");
-		menuSnack[3] = new JButton("떡볶이");
-
-		for (int i = 0; i < menuSnack.length; i++) {
-			menuSnack[i].setFont(kfont);
-			menu1.add(menuSnack[i]);
+		JButton[] bt_snack = new JButton[menu_snack.length];
+		for (int i = 0; i < menu_snack.length; i++) {
+			bt_snack[i] = new JButton(menu_snack[i]); // imageAddr+menu_snack[i]
 		}
 		
+		TextField num[] = new TextField[menu_snack.length];
+		JLabel snackName[] = new JLabel[menu_snack.length];
+		Button minus[] = new Button[menu_snack.length];
+		Button plus[] = new Button[menu_snack.length];
+		JButton ok[] = new JButton[menu_snack.length];
+			
+		for(int i = 0; i< menu_snack.length; i++) {
+			
+			bt_snack[i] = new JButton(menu_snack[i]);
+			if(i<5) {
+				bt_snack[i].setBounds(20+i*150,30,130,130);
+			}else if(i<10) {
+				bt_snack[i].setBounds(20+(i-5)*150,250,130,130);
+			}
+			
+			snackName[i] = new JLabel(menu_snack[i]);
+			snackName[i].setBounds(bt_snack[i].getX()+10,bt_snack[i].getY()-25,115,20);
+			snackName[i].setFont(mfont);
+			snackName[i].setHorizontalAlignment(JLabel.CENTER);
+			snackName[i].setHorizontalTextPosition(JLabel.CENTER);
+			
+			num[i] = new TextField("0");
+			num[i].setBackground(Color.white);
+			num[i].setBounds(bt_snack[i].getX()+40,bt_snack[i].getY()+140,50,20);
+			
+			minus[i] = new Button("-");
+			minus[i].setBounds(bt_snack[i].getX()+10,num[i].getY(),20,20);
+			minus[i].setEnabled(true);
+			minus[i].setFont(mfont);
+			
+			plus[i] = new Button("+");
+			plus[i].setBounds(bt_snack[i].getX()+100,num[i].getY(),20,20);
+			plus[i].setEnabled(true);
+			plus[i].setFont(mfont);
+			
+			ok[i] = new JButton("확인");
+			ok[i].setBounds(bt_snack[i].getX()+15,num[i].getY()+30,100,20);
+			ok[i].setFont(mfont);
+			ok[i].setHorizontalAlignment(JLabel.CENTER);
+			ok[i].setHorizontalTextPosition(JLabel.CENTER);
+			
+			// 메뉴에 컴포넌트 추가
+			menu1.add(ok[i]);
+			menu1.add(bt_snack[i]);
+			menu1.add(snackName[i]);
+			menu1.add(num[i]);
+			menu1.add(minus[i]);
+			menu1.add(plus[i]);
+		}
+
+		menu1.setBounds(0, 100, 800, 700);
+		menu1.setBackground(Color.LIGHT_GRAY);
+		menu1.setVisible(true);
+			
 		menuGUI.add(menu1);
 		return menu1;
-	}
+	} // end addMenu1 ---------------------------------------------------------
 	
 	public JPanel addMenu2() { // 일식
 		
-		JPanel menu2 = new JPanel(new GridLayout(3, 2));
-		menu2.setBounds(0, 100, 800, 700);
-		menu2.setBackground(Color.YELLOW);	
-		menu2.setVisible(false);
+		JPanel menu2 = new JPanel(null);
 		
-		// 일식 메뉴 버튼 생성
-		JButton[] menuJapanese = new JButton[4];
-		menuJapanese[0] = new JButton("초밥");
-		menuJapanese[1] = new JButton("우동");
-		menuJapanese[2] = new JButton("돈까스");
-		menuJapanese[3] = new JButton("모밀");
-
-		for (int i = 0; i < menuJapanese.length; i++) {
-			menuJapanese[i].setFont(kfont);
-			menu2.add(menuJapanese[i]);
+		String menu_japan[] = {"초밥","우동","돈까스","모밀","튀김"};
+		
+		JButton[] bt_japan = new JButton[menu_japan.length];
+		for (int i = 0; i < menu_japan.length; i++) {
+			bt_japan[i] = new JButton(menu_japan[i]); // imageAddr+menu_japan[i]
 		}
+		
+		TextField num[] = new TextField[menu_japan.length];
+		JLabel snackName[] = new JLabel[menu_japan.length];
+		Button minus[] = new Button[menu_japan.length];
+		Button plus[] = new Button[menu_japan.length];
+		JButton ok[] = new JButton[menu_japan.length];
+			
+		for(int i = 0; i< menu_japan.length; i++) {
+			
+			bt_japan[i] = new JButton(menu_japan[i]);
+			if(i<5) {
+				bt_japan[i].setBounds(20+i*150,30,130,130);
+			}else if(i<10) {
+				bt_japan[i].setBounds(20+(i-5)*150,250,130,130);
+			}
+			
+			snackName[i] = new JLabel(menu_japan[i]);
+			snackName[i].setBounds(bt_japan[i].getX()+10,bt_japan[i].getY()-25,115,20);
+			snackName[i].setFont(mfont);
+			snackName[i].setHorizontalAlignment(JLabel.CENTER);
+			snackName[i].setHorizontalTextPosition(JLabel.CENTER);
+			
+			num[i] = new TextField("0");
+			num[i].setBackground(Color.white);
+			num[i].setBounds(bt_japan[i].getX()+40,bt_japan[i].getY()+140,50,20);
+			
+			minus[i] = new Button("-");
+			minus[i].setBounds(bt_japan[i].getX()+10,num[i].getY(),20,20);
+			minus[i].setEnabled(true);
+			minus[i].setFont(mfont);
+			
+			plus[i] = new Button("+");
+			plus[i].setBounds(bt_japan[i].getX()+100,num[i].getY(),20,20);
+			plus[i].setEnabled(true);
+			plus[i].setFont(mfont);
+			
+			ok[i] = new JButton("확인");
+			ok[i].setBounds(bt_japan[i].getX()+15,num[i].getY()+30,100,20);
+			ok[i].setFont(mfont);
+			ok[i].setHorizontalAlignment(JLabel.CENTER);
+			ok[i].setHorizontalTextPosition(JLabel.CENTER);
+			
+			// 메뉴에 컴포넌트 추가
+			menu2.add(ok[i]);
+			menu2.add(bt_japan[i]);
+			menu2.add(snackName[i]);
+			menu2.add(num[i]);
+			menu2.add(minus[i]);
+			menu2.add(plus[i]);
+		}
+		
+		menu2.setBounds(0, 100, 800, 700);
+		menu2.setBackground(Color.LIGHT_GRAY);
+		menu2.setVisible(true);
 		
 		menuGUI.add(menu2);
 		return menu2;
-	}
+	}// end addMenu2 ---------------------------------------------------------
 	
 	public JPanel addMenu3() { // 한식
 		
-		JPanel menu3 = new JPanel(new GridLayout(3, 2));	
-		menu3.setBounds(0, 100, 800, 700);
-		menu3.setBackground(Color.PINK);
-		menu3.setVisible(false);
+		JPanel menu3 = new JPanel(null);		
+		String menu_korean[] = {"테스트1","테스트2","테스트3","테스트4","테스트5"};
 		
-		//양식 메뉴 생성
-		JButton[] menuWestern = new JButton[4];
-		menuWestern[0] = new JButton("스파게티");
-		menuWestern[1] = new JButton("피자");
-		menuWestern[2] = new JButton("햄버거");
-		menuWestern[3] = new JButton("스프");
-
-		for (int i = 0; i < menuWestern.length; i++) {
-			menuWestern[i].setFont(kfont);
-			menu3.add(menuWestern[i]);
+		JButton[] bt_korean = new JButton[menu_korean.length];
+		for (int i = 0; i < bt_korean.length; i++) {
+			bt_korean[i] = new JButton(menu_korean[i]); // imageAddr+menu_korean[i]
 		}
+
+		TextField num[] = new TextField[menu_korean.length];
+		JLabel snackName[] = new JLabel[menu_korean.length];
+		Button minus[] = new Button[menu_korean.length];
+		Button plus[] = new Button[menu_korean.length];
+		JButton ok[] = new JButton[menu_korean.length];
 			
+		for(int i = 0; i< menu_korean.length; i++) {
+			
+			bt_korean[i] = new JButton(menu_korean[i]);
+			if(i<5) {
+				bt_korean[i].setBounds(20+i*150,30,130,130);
+			}else if(i<10) {
+				bt_korean[i].setBounds(20+(i-5)*150,250,130,130);
+			}
+			
+			snackName[i] = new JLabel(menu_korean[i]);
+			snackName[i].setBounds(bt_korean[i].getX()+10,bt_korean[i].getY()-25,115,20);
+			snackName[i].setFont(mfont);
+			snackName[i].setHorizontalAlignment(JLabel.CENTER);
+			snackName[i].setHorizontalTextPosition(JLabel.CENTER);
+			
+			num[i] = new TextField("0");
+			num[i].setBackground(Color.white);
+			num[i].setBounds(bt_korean[i].getX()+40,bt_korean[i].getY()+140,50,20);
+			
+			minus[i] = new Button("-");
+			minus[i].setBounds(bt_korean[i].getX()+10,num[i].getY(),20,20);
+			minus[i].setEnabled(true);
+			minus[i].setFont(mfont);
+			
+			plus[i] = new Button("+");
+			plus[i].setBounds(bt_korean[i].getX()+100,num[i].getY(),20,20);
+			plus[i].setEnabled(true);
+			plus[i].setFont(mfont);
+			
+			ok[i] = new JButton("확인");
+			ok[i].setBounds(bt_korean[i].getX()+15,num[i].getY()+30,100,20);
+			ok[i].setFont(mfont);
+			ok[i].setHorizontalAlignment(JLabel.CENTER);
+			ok[i].setHorizontalTextPosition(JLabel.CENTER);
+			
+			// 메뉴에 컴포넌트 추가
+			menu3.add(ok[i]);
+			menu3.add(bt_korean[i]);
+			menu3.add(snackName[i]);
+			menu3.add(num[i]);
+			menu3.add(minus[i]);
+			menu3.add(plus[i]);
+		}
+		
+		menu3.setBounds(0, 100, 800, 700);
+		menu3.setBackground(Color.LIGHT_GRAY);
+		menu3.setVisible(true);
+		
 		menuGUI.add(menu3);
 		return menu3;
 	}
+	// end addMenu3 ---------------------------------------------------------
 	
-	public JPanel addMenu4() { // 한식
-		
-		JPanel menu4 = new JPanel(new GridLayout(3, 2));		
-		menu4.setBounds(0, 100, 800, 700);
-		menu4.setBackground(Color.CYAN);
-		menu4.setVisible(false);
-		
-		//한식 메뉴 생성
-		JButton[] menuKorean = new JButton[4];
-		menuKorean[0] = new JButton("비빔밥");
-		menuKorean[1] = new JButton("불고기");
-		menuKorean[2] = new JButton("갈비탕");
-		menuKorean[3] = new JButton("육개장");
-
-		for (int i = 0; i < menuKorean.length; i++) {
-			menuKorean[i].setFont(kfont);
-			menu4.add(menuKorean[i]);
+	public JPanel addMenu4() { // 양식
+	
+		JPanel menu4 = new JPanel(null);	
+		String menu_western[] = {"test1","test2","test3","test4","test5"};
+	
+		JButton[] bt_western = new JButton[menu_western.length];
+		for (int i = 0; i < menu_western.length; i++) {
+			bt_western[i] = new JButton(menu_western[i]); // imageAddr+menu_western[i]
 		}
+
+		TextField num[] = new TextField[menu_western.length];
+		JLabel snackName[] = new JLabel[menu_western.length];
+		Button minus[] = new Button[menu_western.length];
+		Button plus[] = new Button[menu_western.length];
+		JButton ok[] = new JButton[menu_western.length];
 		
+		for(int i = 0; i< menu_western.length; i++) {
+		
+			bt_western[i] = new JButton(menu_western[i]);
+			if(i<5) {
+				bt_western[i].setBounds(20+i*150,30,130,130);
+			}else if(i<10) {
+				bt_western[i].setBounds(20+(i-5)*150,250,130,130);
+			}
+		
+			snackName[i] = new JLabel(menu_western[i]);
+			snackName[i].setBounds(bt_western[i].getX()+10,bt_western[i].getY()-25,115,20);
+			snackName[i].setFont(mfont);
+			snackName[i].setHorizontalAlignment(JLabel.CENTER);
+			snackName[i].setHorizontalTextPosition(JLabel.CENTER);
+		
+			num[i] = new TextField("0");
+			num[i].setBackground(Color.white);
+			num[i].setBounds(bt_western[i].getX()+40,bt_western[i].getY()+140,50,20);
+		
+			minus[i] = new Button("-");
+			minus[i].setBounds(bt_western[i].getX()+10,num[i].getY(),20,20);
+			minus[i].setEnabled(true);
+			minus[i].setFont(mfont);
+			
+			plus[i] = new Button("+");
+			plus[i].setBounds(bt_western[i].getX()+100,num[i].getY(),20,20);
+			plus[i].setEnabled(true);
+			plus[i].setFont(mfont);
+		
+			ok[i] = new JButton("확인");
+			ok[i].setBounds(bt_western[i].getX()+15,num[i].getY()+30,100,20);
+			ok[i].setFont(mfont);
+			ok[i].setHorizontalAlignment(JLabel.CENTER);
+			ok[i].setHorizontalTextPosition(JLabel.CENTER);
+			
+			// 메뉴에 컴포넌트 추가
+			menu4.add(ok[i]);
+			menu4.add(bt_western[i]);
+			menu4.add(snackName[i]);
+			menu4.add(num[i]);
+			menu4.add(minus[i]);
+			menu4.add(plus[i]);
+		}	
+	
+		menu4.setBounds(0, 100, 800, 700);
+		menu4.setBackground(Color.LIGHT_GRAY);
+		menu4.setVisible(true);
+	
 		menuGUI.add(menu4);
 		return menu4;
-	}
-	// end addMenus ==========================================================
+	} // end addMenus ==========================================================
 	
 	// 장바구니 패널 추가(JPanel basketGUI) ========================================
 	public void addBasketPanel() {
@@ -297,7 +481,7 @@ public class MainGUI extends JFrame {
 		basketGUI = new JPanel(null);
 		basketGUI.setBounds(0, 650, frameWidth, 250);
 		basketGUI.setVisible(false);
-		basketGUI.setBackground(Color.MAGENTA);	
+		basketGUI.setBackground(Color.WHITE);	
 		
 		// 선택한 메뉴 title, 리스트, 버튼 추가
 		JLabel basketTitle = new JLabel("선택한 메뉴");
@@ -338,11 +522,12 @@ public class MainGUI extends JFrame {
 		
 		// 총 합 텍스트
 		JLabel totalPrice = new JLabel();
+		String[] disp = {"총 합 : ", "0", " 원"};
+		
 		totalPrice.setFont(kfont);
-		totalPrice.setHorizontalTextPosition(JLabel.LEFT);
-
-		String[] disp = {"총 합 : ", "0", "원"};
+		totalPrice.setHorizontalTextPosition(JLabel.RIGHT);
 		totalPrice.setText(disp[0]+disp[1]+disp[2]);
+		totalPrice.setBounds(430, 0, 200, 50);
 		
 		// 객체들을 basketGUI에 추가
 		basketGUI.add(basketTitle);
@@ -367,14 +552,6 @@ public class MainGUI extends JFrame {
 		basketText.setText("장바구니 테스트용 에리어 \n");
 		basketText.append("지금까지 고른 메뉴의 이름과 가격을 확인할 수 있습니다. \n");
 		basketText.append("메뉴를 삭제할 수 있습니다. \n");
-		basketText.append("\n");
-		basketText.append("메뉴를 삭제할 수 있습니다.2 \n");
-		basketText.append("장바구니 테스트용 에리어2 \n");
-		basketText.append("지금까지 고른 메뉴의 이름과 가격을 확인할 수 있습니다.2 \n");
-		basketText.append("메뉴를 삭제할 수 있습니다.3 \n");	
-		basketText.append("장바구니 테스트용 에리어3 \n");
-		basketText.append("지금까지 고른 메뉴의 이름과 가격을 확인할 수 있습니다.3 \n");
-		basketText.append("메뉴를 삭제할 수 있습니다.3 \n");
 
 		// 가격 텍스트창 객체용 ScrollPane 생성
 		int v = ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
@@ -479,8 +656,9 @@ public class MainGUI extends JFrame {
 		
 		JLabel totalPrice = new JLabel();
 		totalPrice.setBounds(frameWidth/2-300/2, 0, 300, 100);
-		totalPrice.setText("결제할 금액 : " + "원");
-		totalPrice.setHorizontalTextPosition(frameHeight);
+		totalPrice.setText("<html><body style='text-align: center'>결제할 금액<br>"+"원</html>");
+		totalPrice.setHorizontalTextPosition(JLabel.CENTER);
+		totalPrice.setHorizontalAlignment(JLabel.CENTER);
 		totalPrice.setFont(new Font("", Font.PLAIN, 34));
 		
 		// creditGUI에 객체 추가
